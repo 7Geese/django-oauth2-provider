5bee75460e73294677a6324168f02fc3c41fa127
Jonas Trappenberg <jonas@7geese.com>
2018-01-03 19:56:23 -0800

    Don't change dictionary size during iteration

--- provider/views.py
+++ provider/views.py
@@ -59,7 +59,7 @@ class AuthUtilMixin(object):
         """
         Clear all OAuth related data from the session store.
         """
-        for key in request.session.keys():
+        for key in list(request.session.keys()):
             if key.startswith(constants.SESSION_KEY):
                 del request.session[key]
 
